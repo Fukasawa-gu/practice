d1c377777ea9c6326b4bd05c0a494cec50ecff90
Fukasawa-gu <38712796+Fukasawa-gu@users.noreply.github.com>
2018-06-01 14:32:20 +0900

    Update yahuoku_watchlist.py

--- yahuoku_watchlist.py
+++ yahuoku_watchlist.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 28 21:10:47 2018
-
 """
 
 from selenium import webdriver
@@ -17,157 +16,144 @@ import time
 try:
     chro = webdriver.Chrome()
 except:
-    chro = webdriver.Chrome('C:/selenium/chromedriver')# chromedriverのpathを通しておく
+    chro = webdriver.Chrome('C:/selenium/chromedriver')# 自分のchromedriverのpathを通しておく
 
-id_list = ["taro", "jiro", "saburo"]
-for i in id_list:
-    #ログイン
-    print("{}でログイン".format(i))
-    name = i
-    chro.get('https://auctions.yahoo.co.jp/')
+account_id = "taro"
 
-    chro.find_element_by_link_text('ログイン').click()
-    time.sleep(1)
+#ログイン
+print("{}でログイン".format(account_id))
+name = account_id
+chro.get('https://auctions.yahoo.co.jp/')
 
-    input = chro.find_element_by_id('username')
-    time.sleep(1)
+chro.find_element_by_link_text('ログイン').click()
+time.sleep(1)
 
-    input.send_keys("{}".format(i))
-    time.sleep(1)
+input = chro.find_element_by_id('username')
+time.sleep(1)
 
-    btn = chro.find_element_by_name('btnNext')
-    time.sleep(1)
+input.send_keys("{}".format(account_id))
+time.sleep(1)
 
-    btn.click()
-    time.sleep(1)
+btn = chro.find_element_by_name('btnNext')
+time.sleep(1)
 
-    input = chro.find_element_by_id('passwd')
-    time.sleep(1)
+btn.click()
+time.sleep(1)
 
-    if i == "taro":
-        input.send_keys('bigbrother')
-        time.sleep(1)
-    elif i == "jiro":
-        input.send_keys('middlebrother')
-        time.sleep(1)
-    else:
-        input.send_keys('littlebrother')
-        time.sleep(1)
+input = chro.find_element_by_id('passwd')
+time.sleep(1)
 
-    btnpass =chro.find_element_by_name('btnSubmit')
-    time.sleep(1)
+input.send_keys('watashihataro')
+time.sleep(1)
 
-    btnpass.click()
-    time.sleep(1)
+btnpass =chro.find_element_by_name('btnSubmit')
+time.sleep(1)
 
-    # ログインしているアカウント以外のリストを作る
-    other_seller = id_list
-    other_seller.remove(i)
-    print(other_seller)
+btnpass.click()
+time.sleep(1)
 
-    # ログインしているアカウント以外を一つずつ見ていく
-    for seller in other_seller:
-        cnt = 1
-        print("{}の商品を見ていきます".format(seller))
-        # sellerの商品一覧の1ページ目に移る
-        print('https://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt))
-        running = True
-        chro.get('https://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt))
+# ログインしているアカウント以外のリストを作る
+other_seller = ['jiro', 'saburo']
+other_seller.remove('taro')
+print(other_seller)
 
+# アカウントを一つずつ見ていく
+for seller in other_seller:
+    cnt = 1
+    print("{}の商品を見ていきます".format(seller))
+    # sellerの商品一覧の1ページ目に移る
+    print('https://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt))
+    running = True
+    chro.get('https://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt))
+
+    # ウォッチリスト登録が終わったid数をカウントする
+    lots_count = 0
+    while(running):
+        
         # 商品IDを得る
-        # ids = chro.find_elements_by_xpath('//*[@id]')
-        # time.sleep(1)
-
-        # ウォッチリスト登録が終わったid数をカウントする
-        lots_count = 0
-        while(running):
-            #chro.get('https://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt))
-            #time.sleep(1)
-            
-            # 商品IDを得る
-            ids = chro.find_elements_by_xpath('//*[@id]')
+        ids = chro.find_elements_by_xpath('//*[@id]')
+        time.sleep(1)
+        lots_id = []
+        for i in ids:
+            id_before = i.get_attribute('id')
+            if ":" in id_before:
+                id_before = id_before.split(':')
+                lots_id.append(id_before[0])
+        # すでにウォッチリスト登録が終わったものをリストから除く
+        lots_id = lots_id[lots_count:]
+        try :
+            print('このページは残り{}個'.format(len(lots_id)))
+            # 商品ページに飛ぶ
+            url_id = 'https://page.auctions.yahoo.co.jp/jp/auction/' + str(lots_id[0])
+            print(url_id)
+            chro.get(url_id)
             time.sleep(1)
-            lots_id = []
-            for i in ids:
-                id_before = i.get_attribute('id')
-                if ":" in id_before:
-                    id_before = id_before.split(':')
-                    lots_id.append(id_before[0])
-            # すでにウォッチリスト登録が終わったものをリストから除く
-            lots_id = lots_id[lots_count:]
-            try :
-                print('このページは残り{}個'.format(len(lots_id)))
-                # 商品ページに飛ぶ
-                url_id = 'https://page.auctions.yahoo.co.jp/jp/auction/' + str(lots_id[0])
-                print(url_id)
-                chro.get(url_id)
+
+            try:
+                # 広告が開かれたら閉じる
+                ad_close = chro.find_element_by_link_text("閉じる")
+                print('ad 発見')
+                time.sleep(1)
+                ad_close.click()
+                time.sleep(1)
+                print('ad 閉じた')
+                # ウォッチリスト登録
+                watch = chro.find_element_by_xpath('//*[@id="l-sub"]/div[1]/ul/li[1]/div/ul/li[3]/a')
+                time.sleep(1)
+                watch.click()
+                time.sleep(1)
+                print("{} をウォッチリストに登録しました".format(lots_id[0]))
+                # 商品一覧に戻る
+                chro.back()
+                time.sleep(1)
+                chro.back()
                 time.sleep(1)
 
+
+            except:
+                # ウォッチリスト登録
+                watch = chro.find_element_by_xpath('//*[@id="l-sub"]/div[1]/ul/li[1]/div/ul/li[3]/a')
+                time.sleep(1)
+                watch.click()
+                time.sleep(1)
+                print("{} をウォッチリストに登録しました".format(lots_id[0]))
+                # 商品一覧に戻る
+                chro.back()
+                time.sleep(1)
+                chro.back()
+                time.sleep(1)
+            # ウォッチリスト登録が一つ終わったのでlots_countに1を加える
+            lots_count += 1
+
+        except Exception as e:
+
+            try:
+                # もし出品数以上のページに飛んだらエラーが出た場合(例：出品数115、飛んだ先のページが151であるとき)
+                # 「条件に一致する検索結果はありません。」が得られればwhile文から抜け出す,idはS1akとS1acがあるのでどちらにも対応しておく
                 try:
-                    # 広告が開かれたら閉じる
-                    ad_close = chro.find_element_by_link_text("閉じる")
-                    print('ad 発見')
+                    end = chro.find_element_by_xpath('//*[@id="S1ak"]/p/em')
                     time.sleep(1)
-                    ad_close.click()
-                    time.sleep(1)
-                    print('ad 閉じた')
-                    # ウォッチリスト登録
-                    watch = chro.find_element_by_xpath('//*[@id="l-sub"]/div[1]/ul/li[1]/div/ul/li[3]/a')
-                    time.sleep(1)
-                    watch.click()
-                    time.sleep(1)
-                    print("{} をウォッチリストに登録しました".format(lots_id[0]))
-                    # 商品一覧に戻る
-                    chro.back()
-                    time.sleep(1)
-                    chro.back()
-                    time.sleep(1)
-
-
+                    running = False
                 except:
-                    # ウォッチリスト登録
-                    watch = chro.find_element_by_xpath('//*[@id="l-sub"]/div[1]/ul/li[1]/div/ul/li[3]/a')
+                    end = chro.find_element_by_xpath('//*[@id="S1ac"]/p/em')
                     time.sleep(1)
-                    watch.click()
-                    time.sleep(1)
-                    print("{} をウォッチリストに登録しました".format(lots_id[0]))
-                    # 商品一覧に戻る
-                    chro.back()
-                    time.sleep(1)
-                    chro.back()
-                    time.sleep(1)
-                # ウォッチリスト登録が一つ終わったのでlots_countに1を加える
-                lots_count += 1
+                    running = False
 
             except Exception as e:
-
-                try:
-                    # もし出品数以上のページに飛んだらエラーが出た場合(例：出品数115、飛んだ先のページが151であるとき)
-                    # 「条件に一致する検索結果はありません。」が得られればwhile文から抜け出す,idはS1akとS1acがあるのでどちらにも対応しておく
-                    try:
-                        end = chro.find_element_by_xpath('//*[@id="S1ak"]/p/em')
-                        time.sleep(1)
-                        running = False
-                    except:
-                        end = chro.find_element_by_xpath('//*[@id="S1ac"]/p/em')
-                        time.sleep(1)
-                        running = False
-
-                except Exception as e:
-                    # 「条件に一致する検索結果はありません。」が得られなければエラーが出てこちらに移る
-                    # 次の50件に移る
-                    cnt += 50
-                    print('このページは全て見終えたのでhttps://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt)+'に移動します。')
-                    chro.get('https://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt))
-                    lots_count = 0
-                    time.sleep(1)
+                # 「条件に一致する検索結果はありません。」が得られなければエラーが出てこちらに移る
+                # 次の50件に移る
+                cnt += 50
+                print('このページは全て見終えたのでhttps://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt)+'に移動します。')
+                chro.get('https://auctions.yahoo.co.jp/seller/'+seller+'&b='+str(cnt))
+                lots_count = 0
+                time.sleep(1)
 
         print('{}は見終わったので次のアカウントをウォッチリスト登録します'.format(seller))
 
-    print('{}は他のアカウントを全て見終わったのでログアウトします。'.format(name))
-    chro.get('https://auctions.yahoo.co.jp/')
-    time.sleep(1)
-    chro.find_element_by_link_text('ログアウト').click()
-    time.sleep(1)
+print('{}はアカウントを全て見終わったのでログアウトします。'.format(name))
+chro.get('https://auctions.yahoo.co.jp/')
+time.sleep(1)
+chro.find_element_by_link_text('ログアウト').click()
+time.sleep(1)
 
-print("すべてのアカウントで相互にウォッチリスト登録終了しました。")
+print("指定したすべてのアカウントの商品をウォッチリスト登録終了しました。")
